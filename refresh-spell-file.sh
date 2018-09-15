2aa63377da4af39635f7b77d6423f093a538da6c
Seth Milliken <seth@araxia.net>
2018-09-15 01:44:26 -0700

    refresh-spell-file: comments and card text
    - Add comments to indicate intentions for each line.
    - Include card rules text in raw list of text used to generate
      spell-file.

--- refresh-spell-file.sh
+++ refresh-spell-file.sh
@@ -3,12 +3,24 @@ add_file="spell/mtg.utf-8.add"
 spl_file="spell/mtg.utf-8.spl"
 transform_file="mtg-spell-functions.vim"
 completion_list="mtg-cardname-completion-list"
+
 curl -O http://mtgjson.com/json/AllCards.json.zip
-unzip -u AllCards.json.zip
-jq -r 'to_entries[] | {"key": .key} | .key' AllCards.json | sort > ${completion_list}
 curl -O http://mtgjson.com/json/SetList.json
+unzip -u AllCards.json.zip
+
+# Card Names
+jq -r 'to_entries[] | {"key": .key} | .key' AllCards.json | sort > ${completion_list}
+
+# Set Names
 jq -r '.[].name' SetList.json | sort >> ${completion_list}
+
+# Set Codes
+jq -r '.[].code' SetList.json | sort >> ${completion_list}
+
+# Card Text
+jq -r 'to_entries[] | {"text": .value.text} | .text' AllCards.json >> ${completion_list}
+
 cp ${completion_list} ${add_file}
-jq -r '.[].code' SetList.json | sort >> ${add_file}
 vim +":so ${transform_file}" +":MakeMtgSpell" +":q" ${add_file}
+
 ls -al ${spl_file}
