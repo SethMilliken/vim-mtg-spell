b6dafac76d239b7962496732600b70237aeded1a
Seth Milliken <>
2018-12-19 23:51:07 -0800

    keep cardname file separate
    - avoid prompts from unzip
    - follow redirects with curl
    - uppercase set names (mtgjson4 only uses lowercase now)

--- refresh-spell-file.sh
+++ refresh-spell-file.sh
@@ -3,24 +3,25 @@ add_file="spell/mtg.utf-8.add"
 spl_file="spell/mtg.utf-8.spl"
 transform_file="mtg-spell-functions.vim"
 completion_list="mtg-cardname-completion-list"
+candidate_list="mtg-raw-word-list"
 
-curl -O http://mtgjson.com/json/AllCards.json.zip
-curl -O http://mtgjson.com/json/SetList.json
-unzip -u AllCards.json.zip
+curl -L -O http://mtgjson.com/json/AllCards.json.zip
+curl -L -O http://mtgjson.com/json/SetList.json
+unzip -f -u AllCards.json.zip
 
 # Card Names
-jq -r 'to_entries[] | {"key": .key} | .key' AllCards.json | sort > ${completion_list}
+jq -r 'to_entries[] | {"key": .key} | .key' AllCards.json | sort | tee ${completion_list} > ${candidate_list}
 
 # Set Names
-jq -r '.[].name' SetList.json | sort >> ${completion_list}
+jq -r '.[].name' SetList.json | sort >> ${candidate_list}
 
 # Set Codes
-jq -r '.[].code' SetList.json | sort >> ${completion_list}
+jq -r '.[].code | ascii_upcase' SetList.json | sort >> ${candidate_list}
 
 # Card Text
-jq -r 'to_entries[] | {"text": .value.text} | .text' AllCards.json >> ${completion_list}
+jq -r 'to_entries[] | {"text": .value.text} | .text' AllCards.json >> ${candidate_list}
 
-cp ${completion_list} ${add_file}
+cp ${candidate_list} ${add_file}
 vim +":so ${transform_file}" +":MakeMtgSpell" +":q" ${add_file}
 
 ls -al ${spl_file}
